ce723027fce570dd0269ca13b1f628fa6fe89599
kudrom <kudrom@riseup.net>
2014-12-28 19:53:46 +0100

    javadoc añadida y eliminada mainActivity

--- app/src/com/asimov/healthwalk/RepositorioLocalizaciones.java
+++ app/src/com/asimov/healthwalk/RepositorioLocalizaciones.java
@@ -17,22 +17,27 @@ import android.location.Location;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.asimov.healthwalk.Utils;
+
 public class RepositorioLocalizaciones {
 	private Context mContext;
 	private final int TAM_BUFFER = 1024;
 	private final String TABLE = "localizaciones";
-	private final String TAG;
 	private String nombre_bd = null;
 	private SQLiteDatabase bd = null;
 
-	RepositorioLocalizaciones(Context contexto, String base_datos, String log_tag){
-		TAG = log_tag;
+	/**
+	 * Unico constructor de la clase
+	 * @param contexto actividad que lo ha llamado
+	 * @param base_datos nombre de la base de datos a crear
+	 */
+	RepositorioLocalizaciones(Context contexto, String base_datos){
 		mContext = contexto;
 		setBaseDatos(base_datos);
 		start();
 	}
 
-	/*
+	/**
 	 * Parar la base de datos, normalmente se hace en el metodo onPause de la 
 	 * actividad que haya instaciado esta clase 
 	 */
@@ -40,11 +45,11 @@ public class RepositorioLocalizaciones {
 		if(bd != null){
 			bd.close();
 			bd = null;
-			Log.d(TAG, "Base de datos cerrada");
+			Log.d(Utils.ASIMOV, "Base de datos cerrada");
 		}
 	}
 	
-	/*
+	/**
 	 * Arrancar la base de datos, normalmente se hace en el metodo onResume
 	 * de la actividad que haya instanciado esta clase
 	 */
@@ -52,13 +57,16 @@ public class RepositorioLocalizaciones {
 		if(nombre_bd != null && bd == null){
 			String path = getPathBD();
 			bd = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
-			Log.d(TAG, "Base de datos abierta desde " + path);
+			Log.d(Utils.ASIMOV, "Base de datos abierta desde " + path);
 		}
 	}
 	
-	/*
+	/**
 	 * Devuelve los centros de salud separados de desde una distancia menor a radio.
 	 * Esta metodo respeta la interfaz de cursores de las consultas sqlite.
+	 * @param desde Centro de la circunferencia de centros de salud
+	 * @param radio Radio de la circunferencia
+	 * @return cursor con los centros de salud pedidos
 	 */
 	public Cursor getCentrosSalud(Location desde, double radio){
 		// TODO: Hay que testearlo con el mapa
@@ -91,8 +99,8 @@ public class RepositorioLocalizaciones {
 		return null;
 	}
 	
-	/*
-	 * Devuelve un string con un numero n de interrogaciones
+	/**
+	 * Usado en getCentrosSalud para construir un string con tantos placeholders como se indique
 	 */
 	private String placeholders(int n){
 		String ret = "";
@@ -106,22 +114,25 @@ public class RepositorioLocalizaciones {
 		return ret;
 	}
 	
-	/*
-	 * Setter de bd
+	/**
+	 * Setter de la base de datos, provoca la copia de la base de datos si no existe.
+	 * @param base_datos Nombre de la base de datos
 	 */
 	public void setBaseDatos(String base_datos){
 		String path_bd = getPathBD(base_datos);
 		nombre_bd = base_datos;
 		if(!existeBaseDatos(path_bd)){
-			Log.d(TAG, "Creando base de datos");
+			Log.d(Utils.ASIMOV, "Creando base de datos");
 			copiarBaseDatos();
 		}else{
-			Log.d(TAG, "La base de datos ya existe");
+			Log.d(Utils.ASIMOV, "La base de datos ya existe");
 		}
 	}
 	
-	/*
+	/**
 	 * Devuelve la ruta en el sistema de ficheros de una base de datos pasada como argumento
+	 * @param base_datos Nombre de la base de datos
+	 * @return Path a la base de datos pasada como argumento
 	 */
 	public String getPathBD(String base_datos){
 		File files_dir = mContext.getFilesDir();
@@ -129,15 +140,16 @@ public class RepositorioLocalizaciones {
 		return data_path.getAbsolutePath() + "/databases/" + base_datos;
 	}
 	
-	/*
+	/**
 	 * Metodo sobrecargado para ofrecer un comportamiento por defecto cuando no se pasa ningun
 	 * argumento a getPathBD(String)
+	 * @return Path a la base de datos interna a la clase
 	 */
 	public String getPathBD(){
 		return getPathBD(nombre_bd);
 	}
 
-	/*
+	/**
 	 * Copia la base de datos del directorio assets al directorio databases de la aplicacion
 	 */
 	private void copiarBaseDatos(){
@@ -146,7 +158,7 @@ public class RepositorioLocalizaciones {
 		try{
 			input = mContext.getAssets().open(nombre_bd);
 		} catch(IOException e){
-			Log.e(TAG, "No se ha encontrado a " + nombre_bd + " en el directorio assets.");
+			Log.e(Utils.ASIMOV, "No se ha encontrado a " + nombre_bd + " en el directorio assets.");
 			nombre_bd = null;
 			Toast toast = Toast.makeText(mContext, R.string.databaseError, Toast.LENGTH_LONG);
 			toast.show();
@@ -157,7 +169,7 @@ public class RepositorioLocalizaciones {
 		String path_bd = getPathBD();
 		File databases = new File(path_bd).getParentFile();
 		if(!databases.exists()){
-			Log.d(TAG, "Creando directorio databases en " + databases.getAbsolutePath());
+			Log.d(Utils.ASIMOV, "Creando directorio databases en " + databases.getAbsolutePath());
 			databases.mkdir();
 		}
 		
@@ -166,7 +178,7 @@ public class RepositorioLocalizaciones {
 		try {
 			output = new FileOutputStream(path_bd);
 		} catch (FileNotFoundException e) {
-			Log.e(TAG, "No se ha podido abrir la base de datos " + nombre_bd + " en " + path_bd);
+			Log.e(Utils.ASIMOV, "No se ha podido abrir la base de datos " + nombre_bd + " en " + path_bd);
 			nombre_bd = null;
 			Toast toast = Toast.makeText(mContext, R.string.databaseError, Toast.LENGTH_LONG);
 			toast.show();
@@ -186,23 +198,23 @@ public class RepositorioLocalizaciones {
 			}
 		} catch(IOException e){
 		}
-		Log.d(TAG, "Base de datos copiada");
+		Log.d(Utils.ASIMOV, "Base de datos copiada");
 		
 		// Cerramos output e input
 		try {
 			output.flush();
 			output.close();
 		} catch (IOException e) {
-			Log.e(TAG, "Error al cerrar output al copiar la base de datos.");
+			Log.e(Utils.ASIMOV, "Error al cerrar output al copiar la base de datos.");
 		}
 		try {
 			input.close();
 		} catch (IOException e) {
-			Log.e(TAG, "Error al cerrar input al copiar la base de datos.");
+			Log.e(Utils.ASIMOV, "Error al cerrar input al copiar la base de datos.");
 		}
 	}
 	
-	/*
+	/**
 	 * Comprueba si existe la base de datos pasada como argumento
 	 */
 	private boolean existeBaseDatos(String path_bd){
