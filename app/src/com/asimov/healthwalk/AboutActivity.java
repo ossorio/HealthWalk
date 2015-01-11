e3c4d7191edf7cb792ace7e9f828fe7ef5ac115f
kudrom <kudrom@riseup.net>
2015-01-11 17:16:29 +0100

    javadoc de stoyan

--- app/src/com/asimov/healthwalk/AboutActivity.java
+++ app/src/com/asimov/healthwalk/AboutActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import com.asimov.healthwalk.R;
 
 
-
+/**
+ * Actividad para mostrar informacion sobre la aplicacion
+ * @author Stoyan Veselinov Andreev
+ *
+ */
 public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
--- app/src/com/asimov/healthwalk/HelpActivity.java
+++ app/src/com/asimov/healthwalk/HelpActivity.java
@@ -3,7 +3,11 @@ package com.asimov.healthwalk;
 import android.app.Activity;
 import android.os.Bundle;
 
-
+/**
+ * Actividad para la ayuda
+ * @author Stoyan Veselinov Andreev
+ *
+ */
 public class HelpActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
