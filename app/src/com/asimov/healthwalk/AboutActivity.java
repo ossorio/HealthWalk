a37d6538ca03cadc03e13b811e21332a589f71b2
kudrom <kudrom@riseup.net>
2015-01-11 17:08:53 +0100

    ahora sí que los cambios se han aplicado

---
+++ app/src/com/asimov/healthwalk/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.asimov.healthwalk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.asimov.healthwalk.R;
+
+
+
+public class AboutActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+    }
+}
